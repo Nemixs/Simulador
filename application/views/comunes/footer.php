55743e84d7b8e3d6e1e3dfdcaaaeacf6dace9d7b
Nemixs <nemixs@hotmail.cl>
2017-10-11 21:28:53 -0300

    Base Trabajo
    Aplicación CodeIgniter

---
+++ application/views/comunes/footer.php
@@ -0,0 +1,58 @@
+    <!-- ============================================
+    FOOTER SECTION
+    =============================================== -->
+    <footer class="footer-wrapper" role="contentinfo">
+      <div class="footer">
+        <div class="pull-right text-muted"><small>Currently v1.2</small></div>
+        <div>Wrapkit Template  &copy; 2015</div>
+      </div>
+    </footer><!-- /.FOOTER -->
+
+  </main><!-- /#MAIN -->
+
+
+  <!-- VENDORS : jQuery & Bootstrap -->
+  <script src="<?php echo base_url() ?>/assets/scripts/vendor.js"></script>
+  <!-- END VENDORS -->
+
+  <!-- DEPENDENCIES : Required plugins -->
+  <script src="<?php echo base_url() ?>/assets/scripts/dependencies.js"></script>
+  <!-- END DEPENDENCIES -->
+
+  <!-- WRAPKIT -->
+  <script src="<?php echo base_url() ?>/assets/scripts/wrapkit.js"></script>
+  <!-- END WRAPKIT -->
+
+  <!-- WRAPKIT SETUPS -->
+  <script src="<?php echo base_url() ?>/assets/scripts/wrapkit-setup.js"></script>
+  <!-- end WRAPKIT SETUPS -->
+
+  <!-- PLUGIN SETUPS: vendors & dependencies setups -->
+  <script src="scripts/plugin-setups.js"></script>
+  <!-- END PLUGIN SETUPS -->
+
+  <!-- COMPONENTS -->
+  <!-- Morris.js + Raphael -->
+  <script src="<?php echo base_url() ?>/assets/scripts/morris.bundle.js"></script>
+  <script src="<?php echo base_url() ?>/assets/scripts/jquery.sparkline.js"></script>
+  <script src="<?php echo base_url() ?>/assets/scripts/jquery.easypiechart.js"></script>
+  <!-- END COMPONENTS -->
+
+
+  <!-- DUMMY: Use for demo -->
+  <script src="<?php echo base_url() ?>/assets/scripts/demo/dashboard1-demo.js"></script>
+
+
+  <!-- Google Analytics: change UA-71722129-1 to be your site's ID. -->
+  <script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='../../../../../www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-71722129-1');ga('send','pageview');
+  </script>
+</body>
+
+<!-- Mirrored from stilearning.com/items/preview/wrapkit/1.2/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 06 Oct 2017 20:44:47 GMT -->
+</html>
