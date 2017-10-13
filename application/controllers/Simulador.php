e8ad60c8c704bdf60b5b0a2695189b88bb39edb7
The black Warrior <themetalrules@gmail.com>
2017-10-13 13:51:41 -0300

    calcularcredito

--- application/controllers/Simulador.php
+++ application/controllers/Simulador.php
@@ -24,4 +24,39 @@ class Simulador extends CI_Controller {
 		$this->load->view('simulador');
 		$this->load->view('comunes/footer');
 	}
+
+	public function calcular(){
+		$monto = $this->input->post('Monto');
+		$num_cuotas = $this->input->post('Cuotas');
+		$fecha = $this->input->post('FechaPago');
+		$primerMes = $this->input->post('PrimerMes');
+		$segundoMes = $this->input->post('SegundoMeso');
+		$CAE = 28.33;
+		$seguros = 152167;
+		$gastosNot = 1111;
+		$total = $monto + $seguros;
+
+//		if($monto > 3000000 && $monto < 6999999){
+			$interes = 0.0182;
+			$cuota = ($total * $interes) / (1 - (1 + $interes) ** $num_cuotas);
+			$total_cred = $cuota * $num_cuotas;
+//		}
+		
+	
+
+		$arrayName = array();
+		$arrayName[0] = $total;
+		$arrayName[1] = $seguros;
+		$arrayName[2] = $gastosNot;
+		$arrayName[3] = $CAE;
+		$arrayName[4] = ($interes*100);
+		$arrayName[5] = $total_cred;
+		$arrayName[6] = $cuota;
+		$arrayName[7] = $primerMes;
+		$arrayName[8] = $segundoMes;
+
+		return $arrayName;
+
+	}
+
 }
